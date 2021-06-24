a2443569e3b54470f3f26aa629c89914ad6cbccb
Alejandro Egas <alejoegasg@outlook.com>
2021-06-24 12:10:41 -0500

    Codigo_Clase 24-06

--- NTP/Modulo_1/Programing/01_Projects/index.js
+++ NTP/Modulo_1/Programing/01_Projects/index.js
@@ -1,10 +1,40 @@
-const express = require('express');
+const express = require("express");
 //genero una aplicacion express
-const app =express()
+const app = express();
 //EndPoint
-app.get('/',(req,res)=>{
-    res.send("Hola mundo con ExpressJSS")
-})
-app.listen(3000,()=>{
-    console.log("Servidor escuchado en http://localhost:3000");
-})
+app.get("/", (req, res) => {
+  res.send("Hola mundo con ExpressJSS");
+});
+app.get("/saludo", (req, res) => {
+  //req.query
+  //console.log(req.query);
+  //Obtener la query del objeto request
+  const {
+    query: { nombre, apellido },
+  } = req;
+  //const nombre = req.query.nombre
+  //const apellido= req.query.apellido
+  /* const {query}=req;
+  res.json({
+    saludo: `Hola soy ${query. nombre} ${query.apellido}`,
+  });
+});
+*/
+  res.json({
+    saludo: `Hola soy ${nombre} ${apellido}`,
+  });
+});
+app.get("/saludo/:nombre", (req, res) => {
+    const {params}=req;
+  /*const {
+    params: { nombre },
+  } = req;
+  //console.log(req.params);
+  //const nombre=req.params.nombre*/
+  res.json({
+    nombre: params.nombre,
+  });
+});
+app.listen(3000, () => {
+  console.log("Servidor escuchado en http://localhost:3000");
+});
---
+++ NTP/Modulo_1/Programing/01_Projects/routes/index.js
@@ -0,0 +1,7 @@
+//Traer el paquete
+const {Router}=require('express');
+//Inicializar Router
+const router =Router()
+//Crear Endpoints
+//Exportar las Rutas
+module.exports.RouterIndex=router
