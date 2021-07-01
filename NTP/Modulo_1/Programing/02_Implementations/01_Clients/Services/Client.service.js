6b75377d7993206970e6a5a6bfa20eae25037eee
Alejandro Egas <alejoegasg@outlook.com>
2021-07-01 13:03:24 -0500

    Codigo Clase_01/07_Autenticación Corregida

--- NTP/Modulo_1/Programing/02_Implementations/01_Clients/Services/Client.service.js
+++ NTP/Modulo_1/Programing/02_Implementations/01_Clients/Services/Client.service.js
@@ -6,7 +6,7 @@ new Promise(async(resolve,reject)=>{
     try {
     const DB= await MongoConnection();
     const clients=DB.collection(COLLECTION);
-    const clientsList=await clients.find({}).ToArray();
+    const clientsList=await clients.find({}).toArray();
     resolve(clientsList);
     } catch (error) {
        reject(error) 
--- NTP/Modulo_1/Programing/02_Implementations/01_Clients/lib/Mongo.js
+++ NTP/Modulo_1/Programing/02_Implementations/01_Clients/lib/Mongo.js
@@ -1,6 +1,6 @@
 const {MongoClient} =require("mongodb");
 const DB_NAME="clients";
-const URL=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}$@modulo-1.n1tbj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const URL=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ntp.b1f5n.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 var MongoConnection=()=>new Promise(async(resolve,reject)=>{
 try {
     let client= new MongoClient(URL,{
